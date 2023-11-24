826000fc15576229dc5d0b53df9d306024d7fa9b
Wesley-waka <wesleywaka77@gmail.com>
2023-11-24 12:04:50 +0300

    styling Featured Products

---
+++ src/components/FeaturedProducts.tsx
@@ -0,0 +1,20 @@
+const FeaturedProducts = () => {
+  return (
+    <Wrapper className="section">
+      <div className="title">
+        <h2>featured products</h2>
+        <div className="underline"></div>
+      </div>
+      <div className="section-center featured">
+        {featured.slice(0, 3).map((product) => {
+          return <Product key={product.id} {...product} />;
+        })}
+      </div>
+      <Link to="/products" className="btn">
+        all products
+      </Link>
+    </Wrapper>
+  );
+};
+
+export default FeaturedProducts;
